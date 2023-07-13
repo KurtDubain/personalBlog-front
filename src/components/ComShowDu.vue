00824d25aa1665bbc62caec83579d0a0c4de693f
KurtDubain <131502852+KurtDubain@users.noreply.github.com>
2023-07-13 23:18:24 +0800

    着重完善了ComShowDu
    主要是完善了ComShowDu组件中的属性名，同时修改了登录判定情况，实现了用户的登录以及创建

--- src/components/ComShowDu.vue
+++ src/components/ComShowDu.vue
@@ -4,11 +4,11 @@
     <div v-if="!isLog">
       <el-form :inline="true" :model="formInline" class="demo-form-inline" label-position="top">
         <el-form-item label="你的昵称" class="little_text" >
-          <el-input v-model="formInline.name" placeholder="不能修改(除非联系博主)" clearable />
+          <el-input v-model="formInline.username" placeholder="不能修改(除非联系博主)" clearable />
         </el-form-item>
         <el-form-item label="联系方式" class="little_text">
           
-          <el-input v-model="formInline.contact" placeholder="邮箱(唯一凭证)" clearable />
+          <el-input v-model="formInline.account" placeholder="邮箱(唯一凭证)" clearable />
         </el-form-item>
         <el-form-item class="big_text">
           <el-input v-model="formInline.content" type="textarea" placeholder="想说点什么呢" clearable />
@@ -41,13 +41,14 @@
           </template>
 
             <div class="text item">等级：{{ userInfo.level }}</div>
+            <el-button @click="showUserInfoCard">查看个人信息</el-button>
             <el-form-item class="big_text">
-              <el-input v-model="formInlineWithLogin.content" type="textarea" placeholder="想说点什么呢" clearable />
+              <el-input v-model="formInline.content" type="textarea" placeholder="想说点什么呢" clearable />
             </el-form-item>
             <el-form-item class="button_row">         
               <el-button type="primary" @click="submitWithLogin" :disabled="isFormInvalid">发布</el-button>
             </el-form-item>
-            <el-button @click="showUserInfoCard">查看个人信息</el-button>
+            
         </el-card>
       </div>
     </div>
@@ -78,22 +79,22 @@ export default {
     const remBtn = ref(true)
     const isLog = ref(false)
     const formInline = reactive({
-      name: '',
-      contact: '',
+      username: '',
+      account: '',
       content: ''
-    })
-    const formInlineWithLogin = reactive({
-      content:'',
     })
     const userInfo = reactive({
       username:'',
-      account:''
+      account:'',
+      likes:0,
+      comments:0,
+      level:1
     })
     // 利用正则表达式判断表单是否为空且是否为正确邮箱格式
     const isFormInvalid = computed(() => {
       return (
         formInline.content.trim() === '' ||
-        !/^\w+([-+.]\w+)*@\w+([-.]\w+)*\.\w+([-.]\w+)*$/.test(formInline.contact)
+        !/^\w+([-+.]\w+)*@\w+([-.]\w+)*\.\w+([-.]\w+)*$/.test(formInline.account)
       )
     })
     // 表单提交功能
@@ -102,8 +103,8 @@ export default {
         const formData = {
           articleId: props.articleId,
           content: formInline.content,
-          name: formInline.name,
-          contact: formInline.contact
+          username: formInline.username,
+          account: formInline.account
         }
         // 将表单内数据由axios发送提交，并由后端补充id、日期等信息
         const res = await axios.post('http://localhost:3000/comments/form', formData)
@@ -114,27 +115,26 @@ export default {
           isLog.value = true
           const resUser = await axios.get(`http://localhost:3000/users/${formInline.name}`)
           console.log('用户数据成功处理',resUser.data)
-          const userInfo = resUser.data
-          localStorage.setItem('userInfo',JSON.stringify({
-            username:userInfo.username,
-            account:userInfo.account,
-            comments:userInfo.comments,
-            likes:userInfo.likes,
-            level:userInfo.level
-          }))
-          EventBus.emit('NeedRefresh')
-          ElMessage.success('评论发表成功')
+          const user = resUser.data;
+          userInfo.username = user.username;
+          userInfo.account = user.account;
+          userInfo.likes = user.likes;
+          userInfo.comments = user.comments;
+          userInfo.level = user.level;
+          localStorage.setItem('userInfo', JSON.stringify(userInfo));
+
         }else{
           formInline.content = ''
-          formInline.name = ''
-          formInline.contact = ''
+          formInline.username = ''
+          formInline.account = ''
           // 使用总线，实现发送一个表单，重新刷新评论表单列表，展示最新数据
-          EventBus.emit('NeedRefresh')
-          ElMessage.success('评论发表成功')
+          
         }
-        
+        EventBus.emit('NeedRefresh')
+        ElMessage.success('评论发表成功')
       } catch (err) {
         console.error('表单数据提交失败', err)
+        ElMessage.error('您的账户或昵称输入有误')
       }
     }
     // 展示个人信息卡片
@@ -196,35 +196,17 @@ export default {
 
     const submitWithLogin = async () => {
       try {
-        // if (!isLog.value) {
-        //   // 未登录状态，进行登录操作
-        //   const loginData = {
-        //     name: formInline.name,
-        //     contact: formInline.contact
-        //   }
-        //   const res = await axios.post('http://localhost:3000/login', loginData)
-        //   if (res.data.success) {
-        //     // 登录成功，更新用户信息和登录状态
-        //     userInfo.username = res.data.username
-        //     userInfo.account = res.data.account
-        //     isLog.value = true
-        //   } else {
-        //     // 登录失败，提示错误信息
-        //     ElMessage.error(res.data.message)
-        //     return
-        //   }
-        // }
-
+        
         // 登录状态下提交评论
         const formData = {
           articleId: props.articleId,
-          content: formInlineWithLogin.content,
-          name: userInfo.username,
-          contact: userInfo.account
+          content: formInline.content,
+          username: userInfo.username,
+          account: userInfo.account
         }
         const res = await axios.post('http://localhost:3000/comments/form', formData)
         console.log('表单数据成功提交', res.data)
-        formInlineWithLogin.contact = ''
+        formInline.content = ''
         EventBus.emit('NeedRefresh')
         ElMessage.success('评论发表成功')
       } catch (err) {
