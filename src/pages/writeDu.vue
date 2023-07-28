c49d63a72b916481f9155c2ac324817d7d500d66
KurtDubain <131502852+KurtDubain@users.noreply.github.com>
2023-07-28 17:07:50 +0800

    补充：write组件防抖
    为了避免在输入src的过程中多次发起请求，就设计了防抖，不过还有问题

--- src/pages/writeDu.vue
+++ src/pages/writeDu.vue
@@ -158,6 +158,7 @@ export default {
         },delay)
       }
     }
+    // 防抖
     const debounceHandleInput = debounce(handleInput,2000)
     const handleInputWithDebounce = () => {
       debounceHandleInput();
