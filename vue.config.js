77ae4a7bb1607573c05719b79b60cd8b5e0b2663
KurtDubain <131502852+KurtDubain@users.noreply.github.com>
2023-09-10 23:27:38 +0800

    修改了 配置文件

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,12 @@
 const { defineConfig } = require('@vue/cli-service')
+
 module.exports = defineConfig({
-  transpileDependencies: true
+  css: {
+    loaderOptions: {
+      sass: {
+        implementation: require('sass'),
+      },
+    },
+  },
+  transpileDependencies: ['vue'], // 添加需要转译的依赖
 })
