5cfd238e57c39a50a2d049d87d7c59d0f1c2dfe4
Du_Yipeng <kurt.du.cobain@gmail.com>
2024-05-17 10:11:13 +0800

    fix:补充了compressor包的遗漏；新增了对审查的要求

--- src/components/footDu.vue
+++ src/components/footDu.vue
@@ -1,60 +1,67 @@
 <!-- 设计的底部栏的组件 -->
 <template>
-    <footer class="footer-container">
-      <div class="footer-content">
-        <div class="footer-links">
-          <router-link to="/" class="footer-link">首页</router-link>
-          <el-divider direction="vertical" />
-          <router-link to="/about" class="footer-link">关于</router-link>
-          <el-divider direction="vertical" />
+  <footer class="footer-container">
+    <div class="footer-content">
+      <div class="footer-links">
+        <router-link to="/" class="footer-link">首页</router-link>
+        <el-divider direction="vertical" />
+        <router-link to="/about" class="footer-link">关于</router-link>
+        <el-divider direction="vertical" />
 
-          <router-link to="/subscription" class="footer-link">联系我们</router-link>
-        </div>
-        <div class="footer-info" >
-          <p>版权所有 &copy; 雪碧</p>
-          <!-- <p>地址：xxx市xxx区xxx街道xxx号</p> -->
-          <p>冀ICP备2023028175号</p>
-          <p>Email：kurt.du.cobain@gmail.com</p>
-        </div>
+        <router-link to="/subscription" class="footer-link"
+          >联系我们</router-link
+        >
       </div>
-    </footer>
-  </template>
-  
-  <script>
-export default{
-    name:"footDu"
-}
+      <div class="footer-info">
+        <p>版权所有 &copy; 雪碧</p>
+        <!-- <p>地址：xxx市xxx区xxx街道xxx号</p> -->
 
+        <p>
+          <a
+            href="https://beian.miit.gov.cn/"
+            target="_blank"
+            style="text-decoration: none; color: #888"
+            >冀ICP备2023028175号</a
+          >
+        </p>
+        <p>Email：kurt.du.cobain@gmail.com</p>
+      </div>
+    </div>
+  </footer>
+</template>
+
+<script>
+export default {
+  name: "footDu",
+};
 </script>
 
-  <style lang="scss" scoped>
-
+<style lang="scss" scoped>
 .footer-container {
-    background-color: #f2f2f2;
-    padding: 20px 0;
-    color: #888;
-  }
-  
-  .footer-content {
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-    justify-content: center;
-  }
-  
-  .footer-links {
-    margin-bottom: 10px;
-  }
-  
-  .footer-link {
-    margin-right: 10px;
-    text-decoration: none;
-    color: #888;
-  }
-  
-  .footer-info {
-    font-size: 14px;
-    text-align: center;
-  }
-  </style>
-  
+  background-color: #f2f2f2;
+  padding: 20px 0;
+  color: #888;
+}
+
+.footer-content {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+}
+
+.footer-links {
+  margin-bottom: 10px;
+}
+
+.footer-link {
+  margin-right: 10px;
+  text-decoration: none;
+  color: #888;
+}
+
+.footer-info {
+  font-size: 14px;
+  text-align: center;
+}
+</style>
